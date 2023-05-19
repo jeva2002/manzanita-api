4320c18df2a103e574fbe4e02cfab79e081080ec
Juan Esteban Villegas Arias <estebanari29@gmail.com>
2023-05-18 20:15:41 -0500

    feat: security basic setud without implementation

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,7 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[UniqueEntity('name')]
 class Category
 {
     #[ORM\Id]
